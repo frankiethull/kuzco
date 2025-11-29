73518cb804a10a262c8f8bf712d9ea2a2c104178
frank hull <frankiethull@gmail.com>
2025-11-29 10:01:15 -0700

    rm depr ellmer chats

--- R/utils-ellmer.R
+++ R/utils-ellmer.R
@@ -5,24 +5,19 @@
 #'
 #' @param provider a provider, such as "ollama", or "claude", or "github"
 #'
-chat_ellmer <- \(provider = "ollama"){
-
-  # fmt: skip
-  chat_providers <- list(
+chat_ellmer <- \(provider = "ollama") {
+	# fmt: skip
+	chat_providers <- list(
     # ~ LOCAL ~
     "ollama"           = ellmer::chat_ollama,
     # ~ CLOUD ~
     "anthropic"        = ellmer::chat_anthropic,
     "aws_bedrock"      = ellmer::chat_aws_bedrock,
-    "azure"            = ellmer::chat_azure,
     "azure_openai"     = ellmer::chat_azure_openai,
-    "bedrock"          = ellmer::chat_bedrock,
     "claude"           = ellmer::chat_claude,
     "cloudfare"        = ellmer::chat_cloudflare,
-    "cortex"           = ellmer::chat_cortex,
     "databricks"       = ellmer::chat_databricks,
     "deepseek"         = ellmer::chat_deepseek,
-    "gemini"           = ellmer::chat_gemini,
     "github"           = ellmer::chat_github,
     "google_gemini"    = ellmer::chat_google_gemini,
     "google_vertex"    = ellmer::chat_google_vertex,
@@ -37,7 +32,7 @@ chat_ellmer <- \(provider = "ollama"){
     "vllm"             = ellmer::chat_vllm
   )
 
- chat_host <- chat_providers[[provider]]
+	chat_host <- chat_providers[[provider]]
 
- return(chat_host)
+	return(chat_host)
 }
