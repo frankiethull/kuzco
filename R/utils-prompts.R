2ae984559350d23cbb15d1b35d1d6b1791b3af8a
frank hull <frankiethull@gmail.com>
2025-12-01 17:45:02 -0700

    doc prompt param for builds

--- R/utils-prompts.R
+++ R/utils-prompts.R
@@ -10,7 +10,7 @@ list_prompts <- \() {
 #' edit prompt
 #' @description
 #' edit a listed prompt installed with kuzco
-#'
+#' @param prompt a prompt from list_prompts()
 #' @export
 edit_prompt <- \(prompt) {
 	file.edit(system.file("prompts", prompt, package = "kuzco"))
